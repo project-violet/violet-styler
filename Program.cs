8cf58a787f581415caec222e5d5c6a3363342ced
violet-dev <violet.dev.master@gmail.com>
2021-04-23 07:31:31 +0000

    (project) Project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace violet_styler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
